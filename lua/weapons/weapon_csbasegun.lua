9719049a776da7125163908621782e15a102f4d9
Jvs <jvstheluacoder@gmail.com>
2014-09-18 11:49:42 +0200

    BaseClass stuff

--- lua/weapons/weapon_csbasegun.lua
+++ lua/weapons/weapon_csbasegun.lua
@@ -2,16 +2,18 @@ AddCSLuaFile()
 
 SWEP.Base = "weapon_csbase"
 
+DEFINE_BASECLASS( SWEP.Base )
+
 SWEP.Spawnable = false
 SWEP.UseHands = true
 SWEP.DrawAmmo = true
 
 function SWEP:Initialize()
-	self.BaseClass.Initialize( self )
+	BaseClass.Initialize( self )
 end
 
 function SWEP:SetupDataTables()
-	self.BaseClass.SetupDataTables( self )
+	BaseClass.SetupDataTables( self )
 	
 	self:NetworkVar( "Float" , 4 , "ZoomFullyActiveTime" )
 	

f98232c430b7a488abcd2762ea8bce6fc291b34d
Jvs <jvstheluacoder@gmail.com>
2014-10-07 10:26:30 +0200

    Started work on the smoke grenade

--- lua/weapons/weapon_smokegrenade.lua
+++ lua/weapons/weapon_smokegrenade.lua
@@ -1,47 +1,48 @@
 AddCSLuaFile()
 DEFINE_BASECLASS( "weapon_basecsgrenade" )
 
-SWEP.ProjectileClass = "ent_flashbang"
+SWEP.ProjectileClass = "ent_smokegrenade"
 
 CSParseWeaponInfo( SWEP , [[WeaponData
 {
-	"MaxPlayerSpeed" 		"250"
-	"WeaponType" 			"Grenade"
-	"WeaponPrice" 			"200"
-	"WeaponArmorRatio"		"1"
-	"CrosshairMinDistance"		"7"
+	"MaxPlayerSpeed"		245
+	"WeaponType"			"Grenade"
+	"WeaponPrice"			"300"
+	"WeaponArmorRatio"		"1.0"
+	"CrosshairMinDistance"		"4"
 	"CrosshairDeltaDistance"	"3"
-	"Team" 				"ANY"
+	"Team"				"ANY"
 	"BuiltRightHanded" 		"0"
 	"PlayerAnimationExtension"	"gren"
-	"MuzzleFlashScale"		"0"
+	"MuzzleFlashScale"		"1"
 	"MuzzleFlashStyle"		"CS_MUZZLEFLASH_NONE"
 	"CanEquipWithShield" 		"1"
-	"AddonModel"			"models/weapons/w_eq_flashbang_thrown.mdl"
-	
+	"AddonModel"			"models/weapons/w_eq_smokegrenade_thrown.mdl"
+		
 	// Weapon characteristics:
-	"Penetration"			"1"
+	"Penetration"			"0"
 	"Damage"			"50"
 	"Range"				"4096"
 	"RangeModifier"			"0.99"
 	"Bullets"			"1"
 	
 	// Weapon data is loaded by both the Game and Client DLLs.
-	"printname"			"#Cstrike_WPNHUD_Flashbang"
-	"viewmodel"			"models/weapons/v_eq_flashbang.mdl"
-	"playermodel"			"models/weapons/w_eq_flashbang.mdl"
+	"printname"			"#Cstrike_WPNHUD_Smoke_Grenade"
+	"viewmodel"			"models/weapons/v_eq_smokegrenade.mdl"
+	"playermodel"			"models/weapons/w_eq_smokegrenade.mdl"
 	
 	"anim_prefix"			"anim"
 	"bucket"			"3"
-	"bucket_position"		"2"
+	"bucket_position"		"3"
 
 	"clip_size"			"-1"
 	"default_clip"			"1"
-	"primary_ammo"			"AMMO_TYPE_FLASHBANG"
+	"primary_ammo"			"AMMO_TYPE_SMOKEGRENADE"
 	"secondary_ammo"		"None"
 
-	"weight"			"1"
+	"weight"			"2"
 	"ITEM_FLAG_EXHAUSTIBLE"		"1"
+// 	"ITEM_FLAG_NOAMMOPICKUPS"	"1"
 
 	// Sounds for the weapon. There is a max of 16 sounds per category (i.e. max 16 "single_shot" sounds)
 	SoundData
@@ -54,17 +55,17 @@ CSParseWeaponInfo( SWEP , [[WeaponData
 		"weapon"
 		{
 				"font"		"CSweaponsSmall"
-				"character"	"G"
+				"character"	"P"
 		}
 		"weapon_s"
 		{	
 				"font"		"CSweapons"
-				"character"	"G"
+				"character"	"P"
 		}
 		"ammo"
 		{
 				"font"		"CSTypeDeath"
-				"character"		"P"
+				"character"		"Q"
 		}
 		"crosshair"
 		{
@@ -106,20 +107,22 @@ CSParseWeaponInfo( SWEP , [[WeaponData
 SWEP.Spawnable = true
 
 function SWEP:EmitGrenade( vecSrc , vecAngles , vecVel , angImpulse , pPlayer )
-	local pGrenade = ents.Create( self.ProjectileClass )
+	local pGrenade = ents.Create( "ent_flashbang" )
 	if not pGrenade then return end
-	
-	pGrenade:SetOwner( pPlayer )
 	pGrenade:SetPos( vecSrc )
 	pGrenade:SetAngles( vecAngles )
-	pGrenade:SetAbsVelocity( vecVel )
-	pGrenade:SetInitialVelocity( vecVel )
+	pGrenade:SetOwner( pPlayer )
 	pGrenade:Spawn()
-	
+	pGrenade:SetVelocity( vecVel )
+	pGrenade:SetInitialVelocity( vecVel )
 	pGrenade:SetThrower( pPlayer )
 	pGrenade:SetDamage( 100 )
 	pGrenade:SetDetonateTimerLength( 1.5 )
-	pGrenade:SetLocalAngularVelocity( angImpulse + pGrenade:GetLocalAngularVelocity() )
+	pGrenade:SetLocalAngularVelocity( angImpulse:Angle() + pGrenade:GetLocalAngularVelocity() )
 	
 end
+
+
+
+
 
